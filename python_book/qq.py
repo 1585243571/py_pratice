b7f293fa9ccb69422b71a10e32332bae6f799d8c
zhangning <zhangning@momenta.ai>
2019-11-27 12:45:04 +0800

    add py books

---
+++ python_book/qq.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import sys;
+'''
+print("import sysy module");
+print(len(sys.argv));
+print(len(sys.path))
+i=()
+o=(1)
+'''
+def printff(a):
+    print(a)
+
+
+
+
+
+if __name__=="__main__":
+    printff("hellow")
+    str='''hejjar
+    arar
+    arar
+    ararar
+    ar
+    ar
+    ar
+    ar\n
+    \n
+    sn\
+    '''
+    print( str)
